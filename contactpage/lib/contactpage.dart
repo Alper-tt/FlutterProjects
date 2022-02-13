c2e92bc2abd8ab0b66a93def0d9c75ab8bb5c8d0
Alper-tt <alpertopraktepe46@gmail.com>
2022-02-13 14:50:56 +0300

    s

--- contactpage/lib/contactpage.dart
+++ contactpage/lib/contactpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(
@@ -7,7 +9,38 @@ void main() {
   );
 }
 
-class BenimUyg extends StatelessWidget {
+class BenimUyg extends StatefulWidget {
+  @override
+  State<BenimUyg> createState() => _BenimUygState();
+}
+
+class _BenimUygState extends State<BenimUyg> {
+  String _instagramurl = 'https://www.instagram.com/alper.t.t/';
+  String _facebookurl = 'https://www.facebook.com/Alper.Topraktepe';
+  String _twitterurl = 'https://twitter.com/alpertt_';
+  String _mail = 'mailto:alpertopraktepe46@gmail.com';
+  String _tel = 'tel:+90-535-943-1412';
+
+  void _launchiURL() async {
+    if (!await launch(_instagramurl)) throw 'Could not launch $_instagramurl';
+  }
+
+  void _launchfURL() async {
+    if (!await launch(_facebookurl)) throw 'Could not launch $_facebookurl';
+  }
+
+  void _launchtURL() async {
+    if (!await launch(_twitterurl)) throw 'Could not launch $_twitterurl';
+  }
+
+  void _launchmail() async {
+    if (!await launch(_mail)) throw 'Could not launch $_mail';
+  }
+
+  void _launchtel() async {
+    if (!await launch(_tel)) throw 'Could not launch $_tel';
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,15 +73,21 @@ class BenimUyg extends StatelessWidget {
                 ),
                 Container(
                   width: 200,
-                    child: Divider(height: 15, color: Colors.deepOrangeAccent[100],thickness: 1,),),
+                  child: Divider(
+                    height: 15,
+                    color: Colors.deepOrangeAccent[100],
+                    thickness: 1,
+                  ),
+                ),
                 Card(
                   margin: EdgeInsets.symmetric(horizontal: 50),
                   color: Colors.deepOrangeAccent[100],
-                  child: ListTile(leading:
-                      Icon(
-                        Icons.email,
-                        color: Colors.black,
-                      ),
+                  child: ListTile(
+                    onTap: _launchmail,
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.black,
+                    ),
                     title: Text('alpertopraktepe46@gmail.com'),
                   ),
                 ),
@@ -57,12 +96,49 @@ class BenimUyg extends StatelessWidget {
                   margin: EdgeInsets.symmetric(horizontal: 50),
                   color: Colors.deepOrangeAccent[100],
                   child: ListTile(
+                    onTap: _launchtel,
                     leading: Icon(
                       Icons.phone,
                       color: Colors.black,
                     ),
                     title: Text('+90(535) 943 14 12'),
                   ),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                      height: 15,
+                      color: Colors.deepOrangeAccent[100],
+                      thickness: 1),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      tooltip: 'İnstagram',
+                      onPressed: _launchiURL,
+                      icon: Icon(
+                        FontAwesomeIcons.instagram,
+                        color: Colors.pink,
+                      ),
+                      splashColor: Colors.deepOrangeAccent,
+                    ),
+                    IconButton(
+                      tooltip: 'Twitter',
+                      onPressed: _launchtURL,
+                      icon: Icon(FontAwesomeIcons.twitterSquare),
+                      splashColor: Colors.deepOrangeAccent,
+                    ),
+                    IconButton(
+                      tooltip: 'Facebook',
+                      onPressed: _launchfURL,
+                      icon: Icon(
+                        FontAwesomeIcons.facebookSquare,
+                        color: Colors.blue,
+                      ),
+                      splashColor: Colors.deepOrangeAccent,
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -72,29 +148,3 @@ class BenimUyg extends StatelessWidget {
     );
   }
 }
-/*
-decoration: BoxDecoration(
-shape: BoxShape.rectangle,
-borderRadius: BorderRadius.circular(20),
-color: Colors.deepOrangeAccent[100],
-),
-margin: EdgeInsets.all(10),
-width: 250,
-height: 40,
-child: Center(
-child: Text('alpertopraktepe46@gmail.com'),
-),
-),
-Container(
-decoration: BoxDecoration(
-shape: BoxShape.rectangle,
-borderRadius: BorderRadiusDirectional.circular(20),
-color: Colors.deepOrangeAccent[100],
-),
-width: 250,
-height: 40,
-child: Center(
-child: Text('+90(535) 943 14 12'),
-),
-),
-*/
